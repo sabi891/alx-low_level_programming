5dde6d0d8a938ff50d6b3f0800a2e4d7b08d9c9f
Saba Tiku <sabatiku89@gmail.com>
2022-03-14 12:58:10 -0700

    My three task commit

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,13 +10,13 @@ int main(void)
 	char lower_case;
 	char upper_case;
 
-	for (upper_case = 'A'; upper_case <= 'z'; upper_case++)
-	{
-		putchar(upper_case);
-	}
 	for (lower_case = 'a'; lower_case <= 'z'; lower_case++)
 	{
-	putchar(lower_case);
+		putchar(lower_case);
+	}
+	for (Upper_case = 'A'; upper_case <= 'z'; upper_case++)
+	{
+	putchar(upper_case);
 	}
 putchar('\n');
 return (0);
