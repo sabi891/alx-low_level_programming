1e3b01d2297225b59b60f3abda95a3ee68c3dfa5
Saba Tiku <sabatiku89@gmail.com>
2022-03-14 05:40:16 -0700

    My six commit task

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,10 +2,10 @@
 /**
  * main - Entry point
  * My hello, printf task
- * Return: return 1 after program execution
+ * Return: 1 after program execution
  */
 int main(void)
 {
-	pritf("with proper grammar, but the outcome ia a piece of art, \n")
-		return (0)
+	pritf("with proper grammar, but the outcome ia a piece of art, \n");
+		return (0);
 }
