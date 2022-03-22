42ea5690494c4c787003267e283eb41b0615bc83
Saba Tiku <sabatiku89@gmail.com>
2022-03-22 14:39:56 -0700

    My commit task

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,7 +10,7 @@ void print_rev(char *s)
 	int i;
 
 	i = 0;
-	while (s[i] != '\0')
+	while (s[i] != '\0' )
 	{
 		i++;
 	}
@@ -20,5 +20,5 @@ void print_rev(char *s)
 		_putchar (s[i]);
 	}
 
-	_putchar ('\n');
+	_putchar ( '\n' );
 }
